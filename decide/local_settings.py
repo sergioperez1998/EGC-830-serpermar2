f797dbccfd8d2553b68610b76050661e63921e5d
sergioperez1998 <sergio-pm@hotmail.com>
2020-09-04 12:53:33 +0200

    Realizando cambios para despliegue la aplicacion en local #1

---
+++ decide/local_settings.py
@@ -0,0 +1,42 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+APIS = {
+    'authentication': 'http://127.0.0.1:8000',
+    'base': 'http://127.0.0.1:8000',
+    'booth': 'http://127.0.0.1:8000',
+    'census': 'http://127.0.0.1:8000',
+    'mixnet': 'http://127.0.0.1:8000',
+    'postproc': 'http://127.0.0.1:8000',
+    'store': 'http://127.0.0.1:8000',
+    'visualizer': 'http://127.0.0.1:8000',
+    'voting': 'http://127.0.0.1:8000',
+}
+
+BASEURL = 'http://127.0.0.1:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_decide2',
+        'USER': 'decide',
+        'PASSWORD': 'decide',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
---
+++ entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jesus/Desktop/EGC/Ejercicios_de_examen/EGC-830/EGC-830-serpermar2/entorno/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
